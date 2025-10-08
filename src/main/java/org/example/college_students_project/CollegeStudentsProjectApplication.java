dd907e4112d8d795dc1b4ee25455aab6318576ed
刘伯聪 <111fsdx@gmail.com>
2025-10-08 15:02:48 +0800

    初始化项目结构

---
+++ src/main/java/org/example/college_students_project/CollegeStudentsProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.college_students_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeStudentsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeStudentsProjectApplication.class, args);
+    }
+
+}
